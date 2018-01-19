a46570d7cf9a444167aeadd8b90ec16b5c68ca7f
Paul <Yipu@appletekiMacBook-Pro.local>
2018-01-18 18:13:53 -0700

    Lab2 commit.

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by Yipu on 18/01/2018.
+ */
+
+public class TweetTooLongException extends Exception {
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by Yipu on 18/01/2018.
+ */
+
+public interface Tweetable {
+
+    public String getMessage();
+
+    public Date getDate();
+
+}
